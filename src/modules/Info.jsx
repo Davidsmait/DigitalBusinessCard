3d8976189b343b83beda4d840574cdff6447108e
Davidsmait <deivod_halo@hotmail.com>
2023-01-17 01:16:23 -0600

    Info section finished

--- src/modules/Info.jsx
+++ src/modules/Info.jsx
@@ -4,11 +4,17 @@ export default function Info (){
     return (
         <>
             <img src="" alt="" className='photo'/>
-            <h2>David San Luis</h2>
-            <h3>Frondend Developer</h3>
-            <h4>DavSan.com</h4>
-            <button>Eamil</button>
-            <button>Linkedin</button>
+            <h2 className='name-card'>David San Luis</h2>
+            <h3 className='potition-name'>Frondend Developer</h3>
+            <h4 className='web-name'>DavSan.com</h4>
+            <button className='email-button'>
+            <i class="fa-solid fa-envelope icon-button"></i>
+                Email
+            </button>
+            <button className='linkedin-button '>
+            <i class="fa-brands fa-linkedin-in icon-button"></i>
+            Linkedin
+            </button>
         </>
     )
 }
